ee6d6184df7bd3a2f4faad14b6142a7818cb9ce1
liuwenjin <liuwenjin@songshuai.com>
2019-12-30 14:54:38 +0800

    新增yx_bullet

--- .hook-apidoc.sh
+++ .hook-apidoc.sh
@@ -7,3 +7,10 @@ fi
 
 
 /usr/bin/apidoc -i $1 -o /data/wwwroot/yx_course_doc/
+
+mkdir /tmp/apidoc
+cp $1 /tmp/apidoc
+cp /data/wwwroot/yx_bullet /tmp/apidoc
+cp /data/wwwroot/yx_bullet/apidoc.json /tmp/apidoc
+cd /tmp/apidoc
+/usr/bin/apidoc -i $1 -o /data/wwwroot/apidoc/
